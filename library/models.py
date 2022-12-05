308058329bb71d670db45045496db15abde9a552
Yuliia Havrylenko <volttabaki@gmail.com>
2022-12-05 16:57:08 +0400

    add models

---
+++ library/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    description = models.CharField()
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
